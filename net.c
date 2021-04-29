1799eecdbbf141cbf5796165e3cb62f0c6c113de
kousuke takahashi <pamarikomi_t@icloud.com>
2021-04-29 22:38:04 +0900

    step4

--- net.c
+++ net.c
@@ -6,9 +6,9 @@
 #include <pthread.h>
 #include <signal.h>
 #include <sys/time.h>
-
 #include "util.h"
 #include "net.h"
+#define NET_THREAD_SLEEP_TIME 1000 /* micro seconds */
 
 struct net_protocol {
   struct net_protocol *next;
@@ -17,16 +17,54 @@ struct net_protocol {
   struct queue_head queue; /* input queue */
   void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
 };
-
-/* NOTE: the data follows immediately after the structure */
 struct net_protocol_queue_entry {
   struct net_device *dev;
   size_t len;
 };
-
 static struct net_protocol *protocols;
-/* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
 static struct net_device *devices;
+
+
+static pthread_t thread;
+static volatile sig_atomic_t terminate;
+
+static void *
+net_thread(void *arg)
+{
+  unsigned int count, num;
+  struct net_device *dev;
+  struct net_protocol *proto;
+  struct net_protocol_queue_entry *entry;
+
+  while (!terminate) {
+    count = 0;
+    for (dev = devices; dev; dev = dev->next) {
+      if (NET_DEVICE_IS_UP(dev)) {
+        if (dev->ops->poll && dev->ops->poll(dev) != -1) {
+          count++;
+        }
+      }
+    }
+    for (proto = protocols; proto; proto = proto->next) {
+      pthread_mutex_lock(&proto->mutex);
+      entry = (struct net_protocol_queue_entry *)queue_pop(&proto->queue);
+      num = proto->queue.num;
+      pthread_mutex_unlock(&proto->mutex);
+      if (entry) {
+        debugf("queue poped (num:%u), dev=%s, type=0x%04x, len=%zd", num, entry->dev->name, proto->type, entry->len);
+        debugdump((uint8_t *)(entry+1), entry->len);
+        proto->handler((uint8_t *)(entry+1), entry->len, entry->dev);
+        free(entry);
+        count++;
+      }
+    }
+    if (!count) {
+      usleep(NET_THREAD_SLEEP_TIME);
+    }
+  }
+  return NULL;
+}
+
 
 struct net_device *
 net_device_alloc(void)
@@ -174,10 +212,17 @@ int
 net_run(void)
 {
   struct net_device *dev;
+  int err;
 
   debugf("open all devices...");
   for(dev = devices; dev; dev = dev->next) {
     net_device_open(dev);
+  }
+  debugf("create background thread...");
+  err = pthread_create(&thread, NULL, net_thread, NULL);
+  if(err) {
+    errorf("pthread_create() failure, err=%d", err);
+    return -1;
   }
   debugf("running...");
   return 0;
@@ -187,7 +232,15 @@ void
 net_shutdown(void)
 {
   struct net_device *dev;
+  int err;
 
+  debugf("terminate background thread...");
+  terminate = 1;
+  err = pthread_join(thread, NULL);
+  if(err) {
+    errorf("pthread_join() failure, err=%d", err);
+    return;
+  }
   debugf("close all devices...");
   for(dev = devices; dev; dev = dev->next) {
     net_device_close(dev);
